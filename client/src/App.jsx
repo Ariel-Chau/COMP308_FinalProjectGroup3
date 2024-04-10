342f2bdd769724e24992b8046c40e470a3f34360
Ariel Zhou <achauzho@my.centennialcollege.ca>
2024-04-10 19:22:09 -0400

    added jwt and bycrpt functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,17 @@ import Signup from './Signup'
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Login from './Login';
+import Home from './Home';
 function App() {
 
   return (
     
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Login />}></Route>
         <Route path='/register' element={<Signup />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/home' element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
 
---
+++ server/index.js
@@ -0,0 +1,79 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const cors = require("cors")
+const UserModel = require('./models/User')
+const bcyrpt = require('bcrypt')
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
+
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST"],
+    credentials: true
+
+}))
+app.use(cookieParser())
+
+mongoose.connect("mongodb://127.0.0.1:27017/comp308_groupassignment");
+
+const verifyUser = (req, res, next) =>{
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.json("The token was not avaialble");
+
+    }else{
+        jwt.verify(token, "jwt-secret-key", (err,decoded)=>{
+            if(err) return res.json("Token is wrong")
+            next();
+        })
+    }
+}
+
+app.get('/home', verifyUser, (req,res)=> {
+    return res.json("Success");
+})
+
+app.post("/login", (req,res) =>{
+    const {email, password} = req.body;
+    UserModel.findOne({email: email})
+    .then(user =>{
+        if(user){
+            bcyrpt.compare(password, user.password, (err, response)=>{
+                
+                if(response){
+                    const token = jwt.sign({email: user.email}, "jwt-secret-key", {expiresIn:"1d"})
+                    res.cookie("token", token);
+                    res.json("Success")
+                }
+                else{
+                    
+                    res.json("The password is incorrect")
+                    
+                }
+            })
+        }else{
+            res.json("No record existed")
+        }
+    })
+})
+
+app.post('/register', (req,res) => {
+    const {name, email, password} = req.body;
+    bcyrpt.hash(password,10)
+    .then(hash => {
+        UserModel.create({name, email, password: hash})
+        .then(users => res.json(users))
+        .catch(err => res.json(err)) 
+    
+    }).catch(err => console.log(err.message));
+    
+})
+
+
+app.listen(3001, () =>{
+    console.log("Server is running")
+})
